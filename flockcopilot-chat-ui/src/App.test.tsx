664abe9a41036a4d1baec72e45bbf0d5f55ffc77
vibeulator <vibeulator@yahoo.com>
2025-12-19 11:44:32 -0500

    Migrate chat UI from CRA to Vite

--- flockcopilot-chat-ui/src/App.test.tsx
+++ flockcopilot-chat-ui/src/App.test.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { test, expect } from 'vitest';
 
 test('renders learn react link', () => {
   render(<App />);
